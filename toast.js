4cb50cc990d5e49b787272d3bbf7918a3c503de5
Aashu260 <aashitadaid26@gmail.com>
2024-11-06 09:34:08 -0700

    1st commit

---
+++ toast.js
@@ -0,0 +1,23 @@
+let isTimerDone = false;
+let isBreadinToaster = false;
+let isToast = false;
+
+const bread = {
+  size: [60, 40],
+  burnPoint: 600,
+  toastPoint: 300,
+};
+
+//toaster variable
+const toaster = {};
+//Functions
+
+//cagedown
+
+//cageup
+
+//timer
+
+//activateHeat
+
+//RemoveToastFromToaster
